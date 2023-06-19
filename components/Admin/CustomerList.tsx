b3cb7290e41944f6b93bd1e3bf06f23bfe697dc5
mozammalrahat <hossain.rahat33@gmail.com>
2023-06-19 21:22:13 +0600

    Add style for Customer list section and add Add customer Added input sections to add a new customer

--- components/Admin/CustomerList.tsx
+++ components/Admin/CustomerList.tsx
@@ -1,12 +1,198 @@
 import React from "react";
 
-const CustomerList: React.FC = () => {
+type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+};
+
+const customers: Customer[] = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "277",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "11212",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "222",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "111",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "212",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "121",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "21",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "18",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "232323",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "1000",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "298",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "100",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "29",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "19",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "27",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "14",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "22",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  {
+    id: "11",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "9876543210",
+  },
+  // Add more demo customers here
+];
+
+const CustomersList: React.FC = () => {
   return (
     <div className="bg-white rounded-lg p-4 shadow">
-      <h2 className="text-xl font-bold mb-4">Customer List</h2>
-      {/* Add your customer list content here */}
+      <h2 className="text-xl font-bold mb-4">Customers List</h2>
+      <div className="max-h-60 overflow-y-auto">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="items-center py-2 px-4 border-b text-center">
+                ID
+              </th>
+              <th className="items-center py-2 px-4 border-b text-center">
+                Phone
+              </th>
+              <th className="items-center py-2 px-4 border-b text-center">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map((customer) => (
+              <tr key={customer.id}>
+                <td className="items-center py-2 px-4 border-b text-center">
+                  {customer.id}
+                </td>
+                <td className="items-center py-2 px-4 border-b text-center">
+                  {customer.phone}
+                </td>
+                <td className="items-center py-2 px-4 border-b text-center">
+                  <button
+                    type="button"
+                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="mt-4">
+        <h3 className="text-lg font-bold mb-2">Add Customer</h3>
+        <div className="flex items-center">
+          <input
+            placeholder="Customer Phone"
+            type="text"
+            className="border border-gray-300 rounded px-4 py-2 mr-2"
+          />
+          <input
+            placeholder="Password"
+            type="text"
+            className="border border-gray-300 rounded px-4 py-2 mr-2"
+          />
+          <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded">
+            Add Customer
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default CustomerList;
+export default CustomersList;
--- components/Admin/Summary.tsx
+++ components/Admin/Summary.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
 const Summary: React.FC = () => {
+  const totalCustomers = 150;
+  const totalOrders = 250;
+  const totalRevenue = 5000;
+
   return (
     <div className="bg-white rounded-lg p-4 shadow">
       <h2 className="text-xl font-bold mb-4">Admin Dashboard Summary</h2>
-      {/* Add your summary content here */}
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-blue-500 text-white p-4 rounded-lg">
+          <h3 className="text-lg font-bold mb-2">Total Customers</h3>
+          <p className="text-3xl font-bold">{totalCustomers}</p>
+        </div>
+        <div className="bg-green-500 text-white p-4 rounded-lg">
+          <h3 className="text-lg font-bold mb-2">Total Orders</h3>
+          <p className="text-3xl font-bold">{totalOrders}</p>
+        </div>
+        <div className="bg-yellow-500 text-white p-4 rounded-lg">
+          <h3 className="text-lg font-bold mb-2">Total Revenue</h3>
+          <p className="text-3xl font-bold">{totalRevenue}$</p>
+        </div>
+      </div>
     </div>
   );
 };
